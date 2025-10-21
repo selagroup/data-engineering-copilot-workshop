fec7d9abf8752129e323a88313b97fb0a24483c2
roee-flowlift <roeel@sela.co.il>
2025-10-21 10:04:11 +0300

    changes

--- Modules/Module_1/Demos/Comments.py
+++ Modules/Module_1/Demos/Comments.py
@@ -1,3 +1,4 @@
+#
 # This script connects to a REST API, fetches JSON data, processes the response,
 # and saves the results into a local CSV file. The goal is to automate data retrieval
 # and analysis from the API. Include error handling, retries, and logging.
